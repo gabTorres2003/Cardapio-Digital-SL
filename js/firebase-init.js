3a84bb1ea3d080b2f5d2c281e77ff56020e3aab4
Gabriel Torres <gabrielstorres08@gmail.com>
2025-06-29 11:11:40 -0300

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -2,13 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAY-Rg98XBj02xCdCbBgdNaLB5k2KQyO_U",
-  authDomain: "cardapio-digital-sl.firebaseapp.com",
-  projectId: "cardapio-digital-sl",
-  storageBucket: "cardapio-digital-sl.firebasestorage.app",
-  messagingSenderId: "1068100672412",
-  appId: "1:1068100672412:web:72c067736280fc4a034659",
-  measurementId: "G-9E65WYJKVG",
+/*
+ Tokens e dados do Google Firebase
+ */
 };
 
 const app = initializeApp(firebaseConfig);
